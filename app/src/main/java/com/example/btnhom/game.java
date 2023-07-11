0cd83e4ee99785107414ee51b012b588890b2f8f
HiepHuynh <huynhhiepvan1998@gmail.com>
2023-07-11 09:36:48 +0700

    game

---
+++ app/src/main/java/com/example/btnhom/game.java
@@ -0,0 +1,165 @@
+package com.example.btnhom;
+
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+        import java.util.Random;
+
+public class game extends AppCompatActivity {
+    int randomNumber;    // Số máy tính phát sinh ngẫu nhiên
+    int guessNumber;    // Số mà người chơi đoán
+    int guessCount = 0;    // Số lần đoán của người chơi
+    String log = "";    // Nhật ký của người chơi
+
+    private TextView txtGuess, txtResult, txtNhatKy, txtViewResult, txtReturn;
+    private Button btnNew, btnGUess, btnFinish;
+    private String checkGuess(int guessNum, int randNum) {
+        String guessStr = Integer.toString(guessNum);
+        String randStr = Integer.toString(randNum);
+
+        // Kiểm tra định dạng đầu vào
+        if (guessStr.length() != randStr.length() || !guessStr.matches("\\d+") || !randStr.matches("\\d+")) {
+            return "Invalid input";
+        }
+
+        int a = 0, b = 0;
+        boolean[] checked = new boolean[3];
+
+        // Tìm các chữ số trùng nhau và đánh dấu là đã kiểm tra
+        for (int i = 0; i < guessStr.length(); i++) {
+            if (guessStr.charAt(i) == randStr.charAt(i)) {
+                a++;
+                checked[i] = true;
+            }
+        }
+
+        // Tìm các chữ số khác nhau và đánh dấu là đã kiểm tra
+        for (int i = 0; i < guessStr.length(); i++) {
+            if (!checked[i]) {
+                for (int j = 0; j < randStr.length(); j++) {
+                    if (!checked[j] && guessStr.charAt(i) == randStr.charAt(j)) {
+                        b++;
+                        checked[j] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return Integer.toString(a) + "?" + Integer.toString(b) + "+";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game);
+
+        txtGuess = findViewById(R.id.txtGuess);
+        txtResult = findViewById(R.id.twResult);
+        txtNhatKy = findViewById(R.id.twNhatky);
+        txtViewResult = findViewById(R.id.twVIewResult);
+        txtReturn = findViewById(R.id.twReturn);
+        btnNew = findViewById(R.id.btnNew);
+        btnGUess = findViewById(R.id.btnGuess);
+        btnFinish = findViewById(R.id.btnFinish);
+
+        btnNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Phat sinh so ngau nhien
+                Random random = new Random();
+                randomNumber = random.nextInt(900) + 100;
+
+                // Xoa noi dung cua cac TextView va EditText
+                txtGuess.setText("");
+                txtResult.setText("");
+                txtNhatKy.setText("");
+                txtViewResult.setText("");
+
+                // Dat gia tri cho cac bien
+                guessCount = 0;
+                log = "";
+
+                // Hien thi thong tin cho nguoi choi
+                Toast.makeText(getApplicationContext(), "Bat dau tro choi moi!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    // Khai báo biến randomStr
+            String randomStr;
+
+    // Sinh số ngẫu nhiên có 3 chữ số
+            Random random = new Random();
+        int randomNumber = random.nextInt(900) + 100;
+        randomStr = String.valueOf(randomNumber);
+        btnGUess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lay gia tri nhap vao tu EditText
+                String guessStr = txtGuess.getText().toString().trim();
+
+                // Kiem tra gia tri nhap vao co hop le hay khong
+                if (guessStr.length() != 3) {
+                    Toast.makeText(getApplicationContext(), "Vui long nhap so co 3 chu so!", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Tang so lan doan len 1
+                    guessCount++;
+
+                    // Chuyen doi chuoi sang so
+                    guessNumber = Integer.parseInt(guessStr);
+
+                    // Kiem tra xem nguoi choi da doan dung hay chua
+                    if (guessNumber == randomNumber) {
+                        txtResult.setText("Chuc mung! Ban da doan dung!");
+                        Toast.makeText(getApplicationContext(), "Chuc mung! Ban da doan dung!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // Phan hoi ket qua
+                        String feedback = "";
+                        for (int i = 0; i < 3; i++) {
+                            if (guessStr.charAt(i) == randomStr.charAt(i)) {
+                                feedback += "+";
+                            } else if (randomStr.contains(String.valueOf(guessStr.charAt(i)))) {
+                                feedback += "?";
+                            }
+                        }
+                        txtResult.setText(feedback);
+
+                        // Luu ket qua vao TextView
+                        txtViewResult.append(guessStr + " - " + feedback + "\n");
+
+                        // Kiem tra so lan doan
+                        if (guessCount >= 7) {
+                            txtResult.setText("Ban da thua, so can doan la " + randomStr);
+                            Toast.makeText(getApplicationContext(), "Ban da thua, so can doan la " + randomStr, Toast.LENGTH_SHORT).show();
+                            btnGUess.setEnabled(false); // Khong cho nguoi choi nhap tiep
+                        }
+                    }
+                }
+            }
+        });
+
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtResult.setText("Game over! So can doan la: " + randomNumber);
+                Toast.makeText(getApplicationContext(), "Game over! So can doan la: " + randomNumber, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //return login page
+//        txtReturn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(game.this,  login.class);
+//                startActivity(intent);
+//            }
+//        });
+    }
+
+}
--- app/src/main/java/com/example/btnhom/register.java
+++ app/src/main/java/com/example/btnhom/register.java
@@ -1,94 +1,98 @@
-package com.example.btnhom;
-
-import android.app.AlertDialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Patterns;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class register extends AppCompatActivity {
-    private EditText nameEditText, emailEditText, passwordEditText, confirmPasswordEditText;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.register);
-
-        // Khởi tạo các trường EditText
-        nameEditText = findViewById(R.id.txtNameRegister);
-        emailEditText = findViewById(R.id.txtEmailRegister);
-        passwordEditText = findViewById(R.id.txtPasswordRegister);
-        confirmPasswordEditText = findViewById(R.id.txtPasswordRR);
-
-        // Thiết lập sự kiện click cho nút đăng kí
-        Button registerButton = findViewById(R.id.btnRegister);
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Lấy giá trị nhập vào từ các trường EditText
-                String name = nameEditText.getText().toString().trim();
-                String email = emailEditText.getText().toString().trim();
-                String password = passwordEditText.getText().toString().trim();
-                String confirmPassword = confirmPasswordEditText.getText().toString().trim();
-
-                // Thực hiện xác thực thông tin người dùng
-                if (TextUtils.isEmpty(name)) {
-                    nameEditText.setError("Please enter your name");
-                    nameEditText.requestFocus();
-                    return;
-                }
-
-                if (TextUtils.isEmpty(email)) {
-                    emailEditText.setError("Please enter your email");
-                    emailEditText.requestFocus();
-                    return;
-                }
-
-                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                    emailEditText.setError("Please enter a valid email");
-                    emailEditText.requestFocus();
-                    return;
-                }
-
-                if (TextUtils.isEmpty(password)) {
-                    passwordEditText.setError("Please enter a password");
-                    passwordEditText.requestFocus();
-                    return;
-                }
-
-                if (password.length() < 6) {
-                    passwordEditText.setError("Password must be at least 6 characters long");
-                    passwordEditText.requestFocus();
-                    return;
-                }
-
-                if (!password.equals(confirmPassword)) {
-                    confirmPasswordEditText.setError("Passwords do not match");
-                    confirmPasswordEditText.requestFocus();
-                    return;
-                }
-
-                // Lưu thông tin người dùng vào cơ sở dữ liệu
-                User newUser = new User(name, email, password);
-                User.listUser.add(newUser);
-                // Code xử lý lưu thông tin người dùng vào cơ sở dữ liệu ở đây
-
-                // Hiển thị thông báo đăng kí thành công
-                Toast.makeText(register.this, "Registration successful", Toast.LENGTH_SHORT).show();
-
-                // Chuyển đến màn hình đăng nhập
-                Intent intent = new Intent(register.this, login.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-    }
-}
-
+//package com.example.btnhom;
+//
+//import android.app.AlertDialog;
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.text.TextUtils;
+//import android.util.Patterns;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.TextView;
+//import android.widget.Toast;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//public class register extends AppCompatActivity {
+//    private EditText nameEditText, emailEditText, passwordEditText, confirmPasswordEditText;
+//    private TextView twAA;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.register);
+//
+//        // Khởi tạo các trường EditText
+//        nameEditText = findViewById(R.id.txtNameRegister);
+//        emailEditText = findViewById(R.id.txtEmailRegister);
+//        passwordEditText = findViewById(R.id.txtPasswordRegister);
+//        confirmPasswordEditText = findViewById(R.id.txtPasswordRR);
+//        twAA = findViewById(R.id.twDaCoTaiKhoang);
+//
+//        // Thiết lập sự kiện click cho nút đăng kí
+//        Button registerButton = findViewById(R.id.btnRegister);
+//        registerButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // Lấy giá trị nhập vào từ các trường EditText
+//                String name = nameEditText.getText().toString().trim();
+//                String email = emailEditText.getText().toString().trim();
+//                String password = passwordEditText.getText().toString().trim();
+//                String confirmPassword = confirmPasswordEditText.getText().toString().trim();
+//
+//                // Thực hiện xác thực thông tin người dùng
+//                if (TextUtils.isEmpty(name) && TextUtils.isEmpty(email) && TextUtils.isEmpty(password)) {
+//                    nameEditText.setError("Please enter your name");
+//                    nameEditText.requestFocus();
+//
+//                    emailEditText.setError("Please enter your email");
+//                    emailEditText.requestFocus();
+//
+//                    passwordEditText.setError("Please enter a password");
+//                    passwordEditText.requestFocus();
+//                    return;
+//                }
+//
+//                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+//                    emailEditText.setError("Please enter a valid email");
+//                    emailEditText.requestFocus();
+//                    return;
+//                }
+//
+//                if (password.length() < 6) {
+//                    passwordEditText.setError("Password must be at least 6 characters long");
+//                    passwordEditText.requestFocus();
+//                    return;
+//                }
+//
+//                if (!password.equals(confirmPassword)) {
+//                    confirmPasswordEditText.setError("Passwords do not match");
+//                    confirmPasswordEditText.requestFocus();
+//                    return;
+//                }
+//
+//                // Lưu thông tin người dùng vào cơ sở dữ liệu
+//                User newUser = new User(name, email, password);
+//                User.listUser.add(newUser);
+//                // Code xử lý lưu thông tin người dùng vào cơ sở dữ liệu ở đây
+//
+//                // Hiển thị thông báo đăng kí thành công
+//                Toast.makeText(register.this, "Registration successful", Toast.LENGTH_SHORT).show();
+//
+//                // Chuyển đến màn hình đăng nhập
+//                Intent intent = new Intent(register.this, login.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
+//
+//        //nhấn để trở về
+//        twAA.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(register.this, login.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
+//}
+//
